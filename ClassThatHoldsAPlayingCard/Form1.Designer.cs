d25865bfa34a9df053391c887859890e8c3d447c
mamaker <madhuvasudevan@hotmail.com>
2015-05-18 22:54:24 -0500

    Done!

--- ClassThatHoldsAPlayingCard/Form1.Designer.cs
+++ ClassThatHoldsAPlayingCard/Form1.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(63, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Playing Card";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- ClassThatHoldsAPlayingCard/Form1.cs
+++ ClassThatHoldsAPlayingCard/Form1.cs
@@ -12,9 +12,17 @@ namespace ClassThatHoldsAPlayingCard
 {
     public partial class Form1 : Form
     {
+        Random random = new Random();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            MessageBox.Show(card.Name);
         }
     }
 }
